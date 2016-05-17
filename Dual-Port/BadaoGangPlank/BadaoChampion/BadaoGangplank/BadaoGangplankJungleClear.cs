522c93026bbf78dab7aba4810e7c20b66005bb77
Pablo Sanchez <jrcrack009@gmail.com>
2016-05-17 13:47:53 -0600

    Badao GP (will test later)

---
+++ Dual-Port/BadaoGangPlank/BadaoChampion/BadaoGangplank/BadaoGangplankJungleClear.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+using EloBuddy.SDK;
+
+namespace BadaoKingdom.BadaoChampion.BadaoGangplank
+{
+    public static class BadaoGangplankJungleClear
+    {
+        public static void BadaoActivate ()
+        {
+            Game.OnUpdate += Game_OnUpdate;
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+                return;
+            if (!BadaoGangplankVariables.JungleQ)
+                return;
+            foreach (Obj_AI_Minion minion in MinionManager.GetMinions(BadaoMainVariables.Q.Range,
+                                                               MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.Health))
+            {
+                if (minion.BadaoIsValidTarget() && BadaoMainVariables.Q.GetDamage(minion) >= minion.Health)
+                {
+                    BadaoMainVariables.Q.Cast(minion);
+                }
+            }
+        }
+    }
+}
